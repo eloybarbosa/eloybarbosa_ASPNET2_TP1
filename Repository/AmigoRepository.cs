c548b76ede958065cecb74e3bf7c618eb582563b
Eloy Barbosa <eloy.barbosa@al.infnet.edu.br>
2020-08-19 01:30:28 -0300

    TP1 Desenvolvimento de Aplicações Web ASP .NET MVC e Web API

--- Repository/AmigoRepository.cs
+++ Repository/AmigoRepository.cs
@@ -1,6 +1,8 @@
-﻿using Domain;
+﻿using Dapper;
+using Domain;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -10,34 +12,50 @@ namespace Repository
 {
     public class AmigoRepository
     {
-        private static List<Amigo> Amigos { get; set; } = new List<Amigo>();
+
+        private string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AmigosDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
 
         public List<Amigo> GetAll()
         {
-            return Amigos;
+            using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+            {
+                return connection.Query<Amigo>("SELECT * FROM AMIGO").ToList();
+            }
+
         }
         public Amigo GetAmigoById(Guid id)
         {
-            return Amigos.FirstOrDefault(x => x.Id == id);
-
+            using(SqlConnection connection = new SqlConnection(this.ConnectionString))
+            {
+                return connection.QueryFirstOrDefault<Amigo>("SELECT * FROM AMIGO WHERE ID = @P1", new { P1 = id });
+            }
         }
 
         public void Save(Amigo amigo)
         {
-            Amigos.Add(amigo);
+            using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+            {
+                connection.Execute(@"INSERT INTO AMIGO(ID, NOME, SOBRENOME, EMAIL, DATANASCIMENTO, STATUS)
+                                            VALUES(@P1,@P2,@P3,@P4,@P5,@P6)
+                ", new { P1 = amigo.Id, P2 = amigo.Nome, P3 = amigo.SobreNome, P4 = amigo.Email, P5 = amigo.DataNascimento, P6 = amigo.Status });
+            }
         }
 
         public Amigo GetAmigoByEmail(string email)
         {
-            return Amigos.FirstOrDefault(x => x.Email == email);
+            using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+            {
+                return connection.QueryFirstOrDefault<Amigo>("SELECT * FROM AMIGO WHERE EMAIL = @P1", new { P1 = email });
+            }
         }
 
-        public void Deletar(Guid Id)
+        public void Deletar(Guid id)
         {
-            var amigo = this.GetAmigoById(Id);
-
-            if (amigo != null)
-                Amigos.Remove(amigo);
+            using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+            {
+                connection.Execute("DELETE FROM AMIGO WHERE ID = @P1", new { P1 = id });
+            }
         }
 
         
